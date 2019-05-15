4f6c9fc773446805e2c50a5b187642b41b9b7467
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-15 15:11:18 -0500

    I got rid of the error but stll worried becaue I don't understand what I am suppose to do

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,18 +1,18 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-}
+
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelectorAll('body .menu');
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector('header .menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+let menuButton = document.querySelector('header .menu-button');
+menuButton.addEventListener('click', function(event)  {
+  return toggleMenu();
 
-menuButton.addEventListener('click', function(event) {
-  event.toggleMenu(click);
-
-}
-)
+})
    
 
+};
