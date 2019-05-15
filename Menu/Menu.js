ed3be2eaa0cda38b7236c84c1fe6295039a22aea
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-15 14:45:10 -0500

    beyond lost

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,7 +4,15 @@ const toggleMenu = () => {
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelectorAll('body .menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('header .menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener('click', function(event) {
+  event.toggleMenu(click);
+
+}
+)
+   
+
